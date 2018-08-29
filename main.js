4e6e513eab665ec81481e966616d26f4d2ce692c
Carolyn Hampe <carolyn.hampe@gmail.com>
2018-08-29 14:35:54 -0700

    tweaked fade in animations

--- main.js
+++ main.js
@@ -6,4 +6,16 @@ $(document).ready(function() {
             scrollTop: $($.attr(this, 'href')).offset().top
         }, 600);
     });
+
+    $(window).scroll( function(i) {
+        $('.fade-in').each( function(i) {
+            let object_height = $(this).outerHeight()/2;
+            let object_top = $(this).offset().top + object_height;
+            let window_bottom = $(window).scrollTop() + $(window).height();
+
+            if(window_bottom > object_top) {
+                $(this).animate({'opacity':'1'}, 500);
+            }
+        });
+    });
 });
